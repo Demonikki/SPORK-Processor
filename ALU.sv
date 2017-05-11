1b6861e5a331c74cab6896b35064d20d0c4aee3a
GLAMDRING\Nik <nsankara@ucsd.edu>
2017-05-11 09:00:59 -0700

    updated ALU and definitions

--- ALU.sv
+++ ALU.sv
@@ -7,97 +7,103 @@ module ALU(
   input        [ 7:0] INPUTA,       // data inputs
                       INPUTB,
                       INPUTC,
-               [ 3:0] INPUTD,
+  input        [ 3:0] INPUTD,
   output logic [ 7:0] OUT,          // or:  output reg [ 7:0] OUT,
   output logic        CO,             // shift or carry out to left (MSB side)
                       ZERO,       // 1: OUT = 0
   output              EQUAL,        // 1: INPUTA = INPUTB
 
   //temporary register
-  reg           [ 4:0] temp,
-                [ 7:0] temp8
+  output reg    [ 4:0] temp,
+  output reg	 [ 7:0] temp8
     );
 
+	 
   assign EQUAL = (INPUTA == INPUTB) ? 1'b1 : 1'b0;   // are inputs equal?
   op_mne op_mnemonic;       // type enum: used for convenient waveform viewing
 
+  
   always_comb begin
   case(OP)
 
     //Simple
-    kADD    : {CO,OUT} = INPUTA+INPUTB+CI;    // A+B+CI
-    kSUB    : {CO,OUT} = INPUTA-INPUTB+CI;  // A-B+CI
+		kADD    : {CO,OUT} = INPUTA+INPUTB+CI;    // A+B+CI
+		kSUB    : {CO,OUT} = INPUTA-INPUTB+CI;  // A-B+CI
     //kSLL    : {CO,OUT} = {INPUTA,CI};     // shift A left, bringing in CI & driving CO
-      kSLL    : {CO, OUT} = INPUTA << 1;
+      
+		kSLL    : {CO, OUT} = INPUTA << 1;
       kSRL    : {CO, OUT} = INPUTA >> 1;
       kSRA    : {CO, OUT} =$signed(INPUTA >>> 1);
       kGT     : {CO, OUT} = INPUTA > INPUTB;
       kLT     : {CO, OUT} = INPUTA < INPUTB;
-      kEQ     : {CO, OUT} = INPUTA == INPUTB;
-      kSLG    : {CO, OUT} = {
-                            temp = INPUTA[7];
-                            INPUTA << 1;
-                            }
+      
+		
+      kSLG    : {CO, OUT} =
+										temp <= INPUTA[7]:
+										INPUTA << 1;
+										
+                            /*
       kSRG    : {CO, OUT} = {
-                            temp = INPUTA[0];
-                            INPUTA >> 1;
+										temp = INPUTA[0];
+										INPUTA >> 1;
                             }
       kSLO    : {CO, OUT} = {
-                            INPUTA << 1;
-                            INPUTA[0] = temp;
+										INPUTA << 1;
+										INPUTA[0] = temp;
                             }
       kSRO    : {CO, OUT} = {
-                            INPUT >> 1;
-                            INPUTA[7] = temp;
+										INPUT >> 1;
+										INPUTA[7] = temp;
                             }
       kNEG    : {CO, OUT} = {
-                            0 - INPUTA;
+										0 + INPUTA;
                             }
       kRLZ    : {CO, OUT} = {
-                            while (INPUTA[7] == 0)
-                                INPUTA << 1;
+										//while (INPUTA[7] == 0)
+											//INPUTA << 1;
                             }
       kSCP    : {CO, OUT} = {
-                            if((INPUTB - INPUTC) > 0){
-                              INPUTB = INPUTB - INPUTC;
-                            } else{
-                              if(INPUTA > 0){
-                                INPUTA = INPUTA - 1;
-                                INPUTB = INPUTB - INPUTC + 255;
-                              } else{
-                                done = 1;
-                              }
-                            }
+										if((INPUTB - INPUTC) > 0){
+											INPUTB = INPUTB - INPUTC;
+										} 
+										else{
+											if(INPUTA > 0){
+												INPUTA = INPUTA - 1;
+												INPUTB = INPUTB - INPUTC + 255;
+											}
+											else{
+												done = 1;
+											}
+										}
                             }
       kSEQ    : {CO, OUT} = {
-                            if(INPUTA[7:4] == INPUTD){
-                              temp = temp + 1;
+										if(INPUTA[7:4] == INPUTD){
+											temp = temp + 1;
                               }
-                            if(INPUTA[6:3] == INPUTD){
-                              temp = temp + 1;
+										if(INPUTA[6:3] == INPUTD){
+											temp = temp + 1;
                               }
-                            if(INPUTA[5:2] == INPUTD){
-                              temp = temp + 1;
+										if(INPUTA[5:2] == INPUTD){
+											temp = temp + 1;
                               }
-                            if(INPUTA[4:1] == INPUTD){
-                              temp = temp + 1;
+										if(INPUTA[4:1] == INPUTD){
+											temp = temp + 1;
                               }
-                            if(INPUTA[3:0] == INPUTD){
-                              temp = temp + 1;
+										if(INPUTA[3:0] == INPUTD){
+											temp = temp + 1;
                               }
-
                             }
       kSQB    : {CO, OUT} = {
-                            if(INPUTA[2:0]+INPUTB[7] == INPUTD){
-                              temp = temp + 1;
-                            }
-                            if(INPUTA[1:0]+INPUTB[7:6] == INPUTD){
-                              temp = temp + 1;
-                            }
-                            if(INPUTA[0]+INPUTB[7:5] == INPUTD){
-                              temp = temp + 1;
-                            }
-      //
+										if(INPUTA[2:0]+INPUTB[7] == INPUTD){
+											temp = temp + 1;
+										}
+										if(INPUTA[1:0]+INPUTB[7:6] == INPUTD){
+											temp = temp + 1;
+										}
+										if(INPUTA[0]+INPUTB[7:5] == INPUTD){
+											temp = temp + 1;
+										}
+      */
     default : {CO,OUT} = 9'b0;
   endcase
   case(OUT)
@@ -108,5 +114,6 @@ module ALU(
     op_mnemonic = op_mne'(OP);
 
   end
+  
 
 endmodule
--- definitions.sv
+++ definitions.sv
@@ -17,26 +17,26 @@ package definitions;
     const logic [3:0]kNEG  = 4'b1011;
     const logic [3:0]kRLZ  = 4'b1100;
     const logic [3:0]kSCO  = 4'b1101;
-    const logic [3:0]kSEQ   = 4'b1110;
+    const logic [3:0]kSEQ  = 4'b1110;
     const logic [3:0]kSQB  = 4'b1111;
 
     typedef enum logic[3:0] {
-        ADD     = 4'h0,
-        SUB     = 4'h1,
-        SLL     = 4'h2,
-        SRL     = 4'h3,
-        SRA     = 4'h4,
-        GT      = 4'h5,
-        LT      = 4'h6,
-        SLG     = 4'h7,
-        SRG     = 4'h8,
-        SLO     = 4'h9,
-        SRO     = 4'h10,
-        NEG     = 4'h11,
-        RLZ      = 4'h12,
-        SCP      = 4'h13,
-        SEQ     = 4'h14,
-        SQB     = 4'h15
+        ADD     = 4'b0000,
+        SUB     = 4'b0001,
+        SLL     = 4'b0010,
+        SRL     = 4'b0011,
+        SRA     = 4'b0100,
+        GT      = 4'b0101,
+        LT      = 4'b0110,
+        SLG     = 4'b0111,
+        SRG     = 4'b1000,
+        SLO     = 4'b1001,
+        SRO     = 4'b1010,
+        NEG     = 4'b1011,
+        RLZ     = 4'b1100,
+        SCP     = 4'b1101,
+        SEQ     = 4'b1110,
+        SQB     = 4'b1111
     } op_mne;
 
 endpackage // defintions
