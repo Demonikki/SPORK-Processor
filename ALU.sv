4e096faee3c1f006d9cb0807923078690a9ef81b
ajays14 <singh.ajay0124@gmail.com>
2017-05-10 18:12:31 -0700

    Needs syntax correction but algorithms are sound

--- ALU.sv
+++ ALU.sv
@@ -1,32 +1,33 @@
 // combinational (unclocked) unsigned ALU w/ carry-in and carry-out
 
-import definitions::*;			      // includes package "definitions"
+import definitions::*;            // includes package "definitions"
 module ALU(
-  input        [ 2:0] OP,		   	  // ALU opcode, part of microcode
+  input        [ 2:0] OP,         // ALU opcode, part of microcode
   input               CI,             // shift or carry in from right (LSB side)
-  input        [ 7:0] INPUTA,	   	  // data inputs
+  input        [ 7:0] INPUTA,       // data inputs
                       INPUTB,
                       INPUTC,
-  output logic [ 7:0] OUT,		      // or:  output reg [ 7:0] OUT,
+               [ 3:0] INPUTD,
+  output logic [ 7:0] OUT,          // or:  output reg [ 7:0] OUT,
   output logic        CO,             // shift or carry out to left (MSB side)
-                      ZERO,			  // 1: OUT = 0
-  output              EQUAL,			  // 1: INPUTA = INPUTB
+                      ZERO,       // 1: OUT = 0
+  output              EQUAL,        // 1: INPUTA = INPUTB
 
   //temporary register
   reg           [ 4:0] temp,
                 [ 7:0] temp8
     );
 
-  assign EQUAL = (INPUTA == INPUTB) ? 1'b1 : 1'b0;	 // are inputs equal?
-  op_mne op_mnemonic;			  // type enum: used for convenient waveform viewing
+  assign EQUAL = (INPUTA == INPUTB) ? 1'b1 : 1'b0;   // are inputs equal?
+  op_mne op_mnemonic;       // type enum: used for convenient waveform viewing
 
   always_comb begin
-	case(OP)
+  case(OP)
 
     //Simple
-	  kADD    : {CO,OUT} = INPUTA+INPUTB+CI;    // A+B+CI
-	  kSUB    : {CO,OUT} = INPUTA-INPUTB+CI;	// A-B+CI
-	  //kSLL    : {CO,OUT} = {INPUTA,CI};			// shift A left, bringing in CI & driving CO
+    kADD    : {CO,OUT} = INPUTA+INPUTB+CI;    // A+B+CI
+    kSUB    : {CO,OUT} = INPUTA-INPUTB+CI;  // A-B+CI
+    //kSLL    : {CO,OUT} = {INPUTA,CI};     // shift A left, bringing in CI & driving CO
       kSLL    : {CO, OUT} = INPUTA << 1;
       kSRL    : {CO, OUT} = INPUTA >> 1;
       kSRA    : {CO, OUT} =$signed(INPUTA >>> 1);
@@ -57,16 +58,52 @@ module ALU(
                                 INPUTA << 1;
                             }
       kSCP    : {CO, OUT} = {
-                            //the difference is in INPUTA
+                            if((INPUTB - INPUTC) > 0){
+                              INPUTB = INPUTB - INPUTC;
+                            } else{
+                              if(INPUTA > 0){
+                                INPUTA = INPUTA - 1;
+                                INPUTB = INPUTB - INPUTC + 255;
+                              } else{
+                                done = 1;
+                              }
+                            }
+                            }
+      kSEQ    : {CO, OUT} = {
+                            if(INPUTA[7:4] == INPUTD){
+                              temp = temp + 1;
+                              }
+                            if(INPUTA[6:3] == INPUTD){
+                              temp = temp + 1;
+                              }
+                            if(INPUTA[5:2] == INPUTD){
+                              temp = temp + 1;
+                              }
+                            if(INPUTA[4:1] == INPUTD){
+                              temp = temp + 1;
+                              }
+                            if(INPUTA[3:0] == INPUTD){
+                              temp = temp + 1;
+                              }
 
                             }
+      kSQB    : {CO, OUT} = {
+                            if(INPUTA[2:0]+INPUTB[7] == INPUTD){
+                              temp = temp + 1;
+                            }
+                            if(INPUTA[1:0]+INPUTB[7:6] == INPUTD){
+                              temp = temp + 1;
+                            }
+                            if(INPUTA[0]+INPUTB[7:5] == INPUTD){
+                              temp = temp + 1;
+                            }
       //
-	  default : {CO,OUT} = 9'b0;
-	endcase
-	case(OUT)
-	  8'b0    : ZERO = 1'b1;
-	  default : ZERO = 1'b0;
-	endcase
+    default : {CO,OUT} = 9'b0;
+  endcase
+  case(OUT)
+    8'b0    : ZERO = 1'b1;
+    default : ZERO = 1'b0;
+  endcase
 //$display("ALU Out %d \n",OUT);
     op_mnemonic = op_mne'(OP);
 
