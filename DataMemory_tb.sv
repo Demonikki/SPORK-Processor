b0317ef146ef3d71d36f7016d2bd903ed4d3b992
GLAMDRING\Nik <nsankara@ucsd.edu>
2017-05-12 16:44:30 -0700

    Finished Data Memory

--- DataMemory_tb.sv
+++ DataMemory_tb.sv
@@ -1,4 +1,4 @@
-module data_mem_tb;
+module DataMemory_tb;
 
     bit
         clk,
@@ -29,39 +29,44 @@ initial begin
 	ReadMem = 0;
 	WriteMem = 0;
 	//starting at 15ns from beginning
-	#15ns;
-	
+	#20ns;
+
 	//Write value 15 into address 0 in memory
 	data_addr = 'b00000000;
 	WriteMem = 1;
 	DataIn = 'b00001111;
-	
+
+   #10ns;
+    
 	//Write value 4 into address 12
 	data_addr = 'b00001100;
 	WriteMem = 1;
 	DataIn = 'b00000100;
-	
+
 	//On the next clock cycle
 	#10ns;
-	
+
 	//Write value 6 into address 12
 	data_addr = 'b00001100;
 	WriteMem = 1;
 	DataIn = 'b00000110;
-	
+
 	//Read from address 0 (should be 15)
 	data_addr = 'b00000000;
 	ReadMem = 1; WriteMem = 0;
 	//DataOut should hold the value 15
-	
+
 	//Next clock cycle
 	#10ns;
-	
+
 	//Read from address 12 (should be 6)
 	data_addr = 'b00001100;
 	ReadMem = 1; WriteMem = 0;
 	//DataOut should hold the value 15
-	
+
+	//stop
+	#10ns $stop;
+
 end
 
 endmodule
