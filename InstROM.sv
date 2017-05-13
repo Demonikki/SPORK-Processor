b2e3b7aaf517d60c2a62b53813e036c908eec023
GLAMDRING\Nik <nsankara@ucsd.edu>
2017-05-12 18:25:51 -0700

    updated PC

--- InstROM.sv
+++ InstROM.sv
@@ -1,34 +1,17 @@
-// Company: 
-// Engineer: 
-// 
-// Create Date:    15:50:22 11/02/2007 
-// Design Name: 
-// Module Name:    InstROM 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: This is a basic verilog module to behave as an instruction ROM
-// 				 template.
-//
-// Dependencies: 
-//
-// Revision: 		2017.02.25
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+
 module InstROM(
-  input       [ 7:0] i_addr,
+  input       [ 15:0] i_addr,
   output 	  [ 8:0] o_value);
 
 logic [8:0] instructions [0:2**7];
-	 
+
  // Instructions have [4bit opcode][3bit rs or rt][2bit rt, immediate, or branch target]
-	 
+
 initial
 begin
 	$readmemb("instruct.txt", instructions);
 end
 
-assign o_value = instructions[i_addr]
+assign o_value = instructions[i_addr];
 
 endmodule
